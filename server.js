1526483a0cdd76382b86893cb89f5dc0efd1ae36
David Sint <david@sint.dev>
2021-03-08 01:21:25 +0000

    v0.0.2 - rewrite
    - This server now uses socket.io v3 instead of v2.
    - This server uses uuid v4 to generate unique IDs
    - Upgrade to esmodules from commonjs
    - Removed unused commented out code
    - Improved logging
    - Code cleanup

--- server.js
+++ server.js
@@ -1,46 +1,57 @@
-const app = require('http').createServer()
-const io = require('socket.io')(app);
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
-app.listen(8080);
+const app = createServer();
+const io = new Server(app, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "/dand\.netlify\.app$/",
+      "/sint\.dev$/"
+    ]
+  }
+});
 
 const getRandom = (max) => {
   return Math.floor(Math.random() * (max) + 1);
 }
 
 io.on('connection', (socket) => {
-  // console.log('a user connected');
-
-  // socket.on('disconnect', reason => {
-  //   console.log('user disconnected');
-  // });
+  socket.on("connect_error", (err) => {
+    console.log(`Connection error due to ${err.message}`);
+  });
 
   socket.on('join game', data => {
-    console.log(`${socket.id} (${data.name}) has joined ${data.game}`);
+    console.log(`ID: ${socket.id} | Name: "${data.name}" has joined game "${data.game}"`);
     socket.join(data.game);
   });
 
   socket.on('leave game', data => {
-    console.log(`${socket.id} (${data.name}) has left ${data.game}`);
+    console.log(`ID: ${socket.id} | Name: "${data.name}" has left game "${data.game}"`);
     socket.leave(data.game)
   });
 
   socket.on('new roll', data => {
-
     const diceThrow = data.dice.map((die) => {
       const newRoll = getRandom(die);
       return {
-        id: `d${die}:${newRoll}@${new Date().getTime()}`,
+        id: `${uuidv4()}`,
         d: die,
         value: newRoll
       }
     })
-    name = data.name || 'Anon'
-    console.log('game:', data.game, ' name:', name, ' roll:', diceThrow);
-
-    io.sockets.to(data.game).emit('receive roll', [{
-      name,
+    const res = {
+      game: data.game,
+      name: data.name || 'Anon',
       roll: diceThrow,
-      mod: data.mod
-    }])
+      mod: data.mod,
+      total: diceThrow.reduce((sum, roll) => sum + roll.value, 0) + data?.mod
+    }
+    console.dir(res, { depth: null });
+
+    io.sockets.to(data.game).emit('receive roll', res)
   });
 });
+
+app.listen(8080);
